7e9751bd4b4f53e71e4861bf1caa6a4d047662f2
Austin Hinderer <zeshon@gmail.com>
2015-01-26 23:02:19 -0800

    Added jsHint

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,11 +20,16 @@ module.exports = function(grunt) {
         src: ['src/js/_header.js', 'src/js/audio_controls.js', 'src/js/video.js', 'src/js/player_utils.js', 'src/js/player.js', 'src/js/_footer.js'],
         dest: 'build/js/player.js'
       }
+    },
+    jshint: {
+      beforeconcat: ['src/js/audio_controls.js', 'src/js/video.js', 'src/js/player_utils.js', 'src/js/player.js'],
+      afterconcat: ['build/js/player.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('build:dev', ['concat:dev', 'compass']);
   grunt.registerTask('default', ['build:dev']);
