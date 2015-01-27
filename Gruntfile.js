026ca136061a52ebda1081ef3bdb74d572f7af69
Austin Hinderer <zeshon@gmail.com>
2015-01-26 22:04:08 -0800

    Added a folder for holding example videos. Pointed the example at the folder.
    Added SCSS + Compass compilation to Grunt. Updated example asset paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,31 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
   grunt.initConfig({
+    compass: {
+      dev: {
+        options: {
+          cssDir: 'build/css',
+          environment: 'development',
+          imagesDir: 'images',
+          relativeAssets: true,
+          sassDir: 'src/scss',
+          sourcemap: true,
+          trace: true
+        }
+      }
+    },
     concat: {
       dev: {
-        src: ['src/_header.js', 'src/audio_controls.js', 'src/video.js', 'src/player_utils.js', 'src/player.js', 'src/_footer.js'],
-        dest: 'build/player.js'
+        src: ['src/js/_header.js', 'src/js/audio_controls.js', 'src/js/video.js', 'src/js/player_utils.js', 'src/js/player.js', 'src/js/_footer.js'],
+        dest: 'build/js/player.js'
       }
     }
   });
 
-  grunt.registerTask('build:dev', ['concat:dev']);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.registerTask('build:dev', ['concat:dev', 'compass']);
   grunt.registerTask('default', ['build:dev']);
 };
